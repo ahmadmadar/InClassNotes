c2f8b861d0a4b45ddeddfdd76c57e3a3387c4f30
ahmadmadar <35266736+ahmadmadar@users.noreply.github.com>
2018-03-16 11:50:13 -0600

    Created Northwind Demos

---
+++ Northwind Demos/NorthwindEntities/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindEntities
+{
+    [Table("Categories")]
+    public class Category
+    {
+        [Key] // "Annotation" - Primary Key
+        public int CategoryID { get; set; }
+
+        [Required, StringLength(15)]
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public byte[] Picture { get; set; }
+
+        [StringLength(40)]
+        public string PictureMimeType { get; set; }
+
+    }
+}
---
+++ Northwind Demos/NorthwindEntities/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindEntities
+{
+    [Table("Products")] // Attribute
+    public class Product
+    {
+        public int ProductID { get; set; }
+        [Required] 
+        [StringLength(40)]
+        public string ProductName { get; set; }
+        public int? SupplierID { get; set; }
+        public int? CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short? UnitsInStock { get; set; }
+        public short? UnitsOnOrder { get; set; }
+        public short? ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+
+        [NotMapped] // tells EF to NOT associate this property with a specific column in the database.
+        public string ProductInfo
+        {
+            get
+            {
+                string info;
+                if (!string.IsNullOrWhiteSpace(QuantityPerUnit))
+                    info = ProductName;
+                else
+                    info = $"{ProductName} ({QuantityPerUnit})";
+                return info;
+            }
+        }
+    }
+}
